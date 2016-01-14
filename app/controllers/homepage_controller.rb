df3540377524701adb95883b7e40582a1d6337f1
Asfand Yar Qazi <ayqazi@gmail.com>
2016-01-14 11:52:16 +0000

    Homepage now renders when post body is nil

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,6 @@
 class HomepageController < ApplicationController
   def index
     @post = Post.first || Post.new
-    @post.set_body_formatter { |p| p.gsub(/\r?\n/, "<br>") }
+    @post.set_body_formatter { |p| p.to_s.gsub(/\r?\n/, "<br>") }
   end
 end
