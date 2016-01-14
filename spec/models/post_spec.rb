af9d88b36df21ceb9e70b95f576e3040e95f1108
Asfand Yar Qazi <ayqazi@gmail.com>
2016-01-14 11:35:43 +0000

    Post allows optional formatting of body

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Post do
 
-  it "works" do
+  it "fills in posted_on time" do
     post = Post.create! title: 'Foo', body: 'Bar'
     expect(post.posted_on).to be_present
+  end
+
+  it "allows setting a custom body formatter to override the default body formatter" do
+    p = Post.new
+    p.body = "post_body"
+    expect(p.formatted_body).to eq "post_body"
+
+    p.set_body_formatter { |body| "formatted:#{body}"  }
+    expect(p.formatted_body).to eq "formatted:post_body"
   end
 
 end
